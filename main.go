e9757e32a973e6f83e6feaab7338d80d38ac26e4
Alexander Denisov <a.v.denisov@gmail.com>
2016-11-09 14:10:43 +0300

    add echo

--- main.go
+++ main.go
@@ -21,4 +21,30 @@ func main() {
        log.Panic(err)
     }
     fmt.Println(configuration.TelegramBotToken)
+
+bot, err := tgbotapi.NewBotAPI(configuration.TelegramBotToken)
+
+if err != nil {
+    log.Panic(err)
+}
+
+bot.Debug = false
+
+log.Printf("Authorized on account %s", bot.Self.UserName)
+
+u := tgbotapi.NewUpdate(0)
+u.Timeout = 60
+
+updates, err := bot.GetUpdatesChan(u)
+
+if err != nil {
+    log.Panic(err)
+}
+// В канал updates будут приходить все новые сообщения.
+for update := range updates { 
+    // Создав структуру - можно её отправить обратно боту
+    msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+    msg.ReplyToMessageID = update.Message.MessageID
+    bot.Send(msg)
+}
 }
